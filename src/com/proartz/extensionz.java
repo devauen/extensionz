29a2404381135016498a261925abd5bb70823c56
Marcin Filipowski <proartz.devon@gmail.com>
2019-02-26 14:26:53 +0100

    Add a find extensions path

---
+++ src/com/proartz/extensionz.java
@@ -0,0 +1,8 @@
+package com.proartz;
+
+public class extensionz {
+    public static void main(String[] args) {
+        String ext = System.getProperty("java.ext.dirs");
+        System.out.println(ext);
+    }
+}
